0c57452a7e8f49283bbe4ddd9d2e2e98b541c69a
Xnowly <147676111+Xnowly@users.noreply.github.com>
2024-04-04 11:25:32 +0800

    Add files via upload

---
+++ meet2.py
@@ -0,0 +1,21 @@
+#variabel
+a = 9
+print (a)
+print ("nilai sy adalah =", a)
+
+
+#belajar intup 
+e = 9
+print(" nilai saya adalah ", e)
+
+#intup2
+#kenapa py
+
+e = 6
+
+print ("nilai saya adalah", e)
+#variabel& value
+z = e
+print("nilai saya yang baru", z)
+
+
