32b666c26c311d3a8c832463d7e7a7ecdfef8973
Xnowly <147676111+Xnowly@users.noreply.github.com>
2024-05-16 14:16:18 +0800

    Add files via upload

---
+++ meet 6.py
@@ -0,0 +1,38 @@
+# perbandingan
+# lebh dari >
+# kurang dari <
+# lebih dari sama dengan >=
+# kurang dari sama dengan <=
+# sama dengan ==
+# tidak sama dengan!=
+# is
+# is not
+
+a = 3
+b = 6
+
+hasil = a > b 
+print (a, '>' , b, '=' , hasil)
+
+hasil = a < b 
+print (a, '<' , b, '=' , hasil)
+
+
+hasil = a >= b 
+print (a, '>=' , b, '=' , hasil)
+
+hasil = a <= b 
+print (a, '<=' , b, '=' , hasil)
+
+hasil = a == b 
+print (a, '==' , b, '=' , hasil)
+
+
+hasil = a != b 
+print (a, '!= ' , b, '=' , hasil)
+
+hasil = a is b # angka yg sama yaitu 3 jika angka nya berbeda akan false
+print (a, 'is' , b, '=' , hasil)
+
+hasil = a is not b #ini kebalikan nya, jika angkanya sama variabel nya sama maka dia akan false jika angka nya berbeda maka hasil nya true
+print (a, 'is not' , b, '=' , hasil)
