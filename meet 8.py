47b66e3d6567a8ca7b31833190de22a2992bc95c
Xnowly <147676111+Xnowly@users.noreply.github.com>
2024-06-07 02:52:25 +0800

    Add files via upload

---
+++ meet 8.py
@@ -0,0 +1,21 @@
+#percabangan jika dan maka
+nama = input ( "silahkan masukan nama ")
+#if jika memenuhi kondisi : aksi
+#if nama == "putra" : print ("maha siswa rajin :v")
+#print ("program selesai")
+
+#if nama == "putra":
+#   print ("maha siswa rajin :v")
+#   print ("putra ror  ") 
+#   print ("maha siswa rajin :v")
+#   print (f"thaks {edy}")
+
+#if else
+if nama == "putra":
+  print ("maha siswa rajin :v")
+  print ("kiw kiw kiw")
+  
+else :
+  print ("lu siapa bjir ? ")
+  print ("bukan putra")
+
