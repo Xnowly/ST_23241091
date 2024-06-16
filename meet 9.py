277d0b3a6326c673bbb9c09bd407052cb27163b9
Xnowly <147676111+Xnowly@users.noreply.github.com>
2024-06-16 20:52:00 +0800

    Add files via upload

---
+++ meet 9.py
@@ -0,0 +1,12 @@
+#Belajar multi percabangan
+
+nama = input ("masukan nama:") 
+if nama=="putra":#kondisi 1 jika memenuhi kondisi 1 (true)
+  print ("maha siswa rajin :v")#aksi true jika kondisi terpenuhi
+elif nama=="udin":#kondisi 2 
+  print ("Akwokakwok udin")#aksi 2
+elif nama=="asep":#aksi 3
+  print ("asep kendaraan")#aksi true
+else : #komdisi 4
+  print ("lu siapa ? ")#false jika tidak memenuhi semua kondisi
+  print ("program selesai")#end program
